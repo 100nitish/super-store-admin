891fd9b60c5dcc1c492607fa4e3ca98849473ede
nitish <mauryanitish510@gmail.com>
2024-12-24 08:32:48 +0530

    icommit

--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const  getAllUsers  = require("../controllers/admin-controller");
+const authMiddleware = require("../middleware/auth-middleware")
+
 const router = express.Router();
 
-router.route('/users').get(getAllUsers); 
+router.route('/users').get(authMiddleware,getAllUsers); 
 
 module.exports = router
 
